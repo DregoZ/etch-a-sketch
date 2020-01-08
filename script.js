e20a90fb50b2b210a9f047fa266e239657f70c49
DregoZ <xanton.ac@gmail.com>
2020-01-08 13:58:28 +0100

    Falta botón clear
    Pintar, borrar, color rdy!

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ window.onload = function () {
     pintar = document.getElementById("pintar");
     color = document.getElementById("color");
     borrar = document.getElementById("borrar");
+    clear = document.getElementById("clear");
 
     tablero.addEventListener("click", activarPincel);
     pintar.addEventListener("click", crearTablero);
     borrar.addEventListener("click", borrarTablero);
+    clear.addEventListener("click", clearBoard);
+    
+}
+
+function clearBoard() {
+    /* estamos trabajando en ello */
 }
 
 function activarPincel() {
@@ -27,27 +34,42 @@ function borrarTablero() {
     }
 }
 
-
-
 function crearTablero() {
-    let tamaño = prompt("Tamaño del lienzo (máximo 100");
+    let tamano = prompt("Tamaño del lienzo (max. 100");
+
+    if (tamano < 1) {
+        alert("Tamaño mínimo 1");
+        tamano = 1;
+    }
+
+    if (tamano > 100) {
+        tamano = 100;
+        alert("Tamaño máximo 100");
+    }
 
     borrarTablero();
 
-    for (let x = 0; x < tamaño; x++) {
+    for (let x = 0; x < tamano; x++) {
         var fila = document.createElement("div");
         fila.setAttribute("id", "fila-" + x);
         tablero.appendChild(fila);
 
-        for (let y = 0; y < tamaño; y++) {
+        for (let y = 0; y < tamano; y++) {
+         //   let currentVal = 'white';
+
             var celda = document.createElement("div");
             celda.setAttribute("id", "celda-" + x + "-" + y);
             celda.setAttribute("class", "celda");
-            celda.setAttribute("style", "width:" + ((750 - tamaño * 2) / tamaño) + "; height:" + ((750 - tamaño * 2) / tamaño));
+            celda.setAttribute("value", "white");
+            celda.setAttribute("style", "width:" + ((750 - tamano * 2) / tamano) + "; height:" + ((750 - tamano * 2) / tamano) + "; background-color:" + celda.getAttribute('value'));
+
+
 
             celda.addEventListener("mouseover", function () {
+                celda.setAttribute("value", color.value);
+
                 if (pintura) {
-                    this.setAttribute("class", "pintado");
+                    this.setAttribute("style", "width:" + ((750 - tamano * 2) / tamano) + "; height:" + ((750 - tamano * 2) / tamano) + "; background-color:" + celda.getAttribute('value'));
                 }
 
             });
@@ -58,6 +80,11 @@ function crearTablero() {
 
     }
 
+
+    function newFunction() {
+        let a = celda.value;
+        console.log("valor:" + a);
+    }
 }
 
 
